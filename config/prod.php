78f8fa601cc37d740a76e9edfcb44e8186205f80
tam24 <tam.escalante@gmail.com>
2017-12-12 15:26:09 +0100

    Models\ classes creation,  src\controllers.php routing added, prod.php db connection

--- config/prod.php
+++ config/prod.php
@@ -1,6 +1,38 @@
 <?php
 
 // configure your app for the production environment
+use Dflydev\Provider\DoctrineOrm\DoctrineOrmServiceProvider;
+use Silex\Provider\SecurityServiceProvider;
+use Symfony\Component\Security\Core\Encoder\PlaintextPasswordEncoder;
 
 $app['twig.path'] = array(__DIR__.'/../templates');
 $app['twig.options'] = array('cache' => __DIR__.'/../var/cache/twig');
+
+$app->register(
+    new Silex\Provider\DoctrineServiceProvider(),
+    [
+        'db.options' => [
+            'driver'   => 'pdo_mysql',
+            'dbname'   => 'michelm_sql0',
+            'host'     => 'wf3.progweb.fr/mysqlpma',
+            'user'     => 'michelm',
+            'password' => 'webforce3'
+        ]
+    ]
+);
+
+$app->register(
+    new DoctrineOrmServiceProvider(),
+    [
+        'orm.proxies.dir' => sys_get_temp_dir(),
+        'orm.em.options' => [
+            'mappings' => [
+                [
+                    'type' => 'annotation',
+                    'namespace' => 'Models',
+                    'path' => __DIR__.'/../src/Models'
+                ]
+            ]
+        ]
+    ]
+);
---
+++ src/Models/Product.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace Models;
+
+/**
+ * @Entity()
+ * @Table(name="product")
+ */
+class Product {
+    /**
+     * @id()
+     * @GeneratedValue()
+     * @Column(name="prod_id", type="integer", nullable=false)
+     */
+    protected $id;
+    
+    /**
+     * @Column(name="prod_name", type="string", length=45, nullable=false)
+     */
+    private $name;
+    
+    /**
+     * @Column(name="prod_description", type="string", length=255, nullable=false)
+     */
+    private $description;
+    
+    /**
+     * @Column(name="prod_price", type="float", nullable=false)
+     */
+    private $price;
+    
+    /**
+     * @Column(name="prod_stock", type="integer", nullable=false)
+     */
+    private $stock;
+    
+    /**
+     * @Column(name="prod_vat", type="integer", nullable=false)
+     */
+    private $vat;
+    
+    /**
+     * @Column(name="prod_cat_id", type="integer", nullable=false)
+     */
+    private $catid;
+    
+    public function getId() {
+        return $this->id;
+    }
+
+    public function getName() {
+        return $this->name;
+    }
+
+    public function getDescription() {
+        return $this->description;
+    }
+
+    public function getPrice() {
+        return $this->price;
+    }
+
+    public function getStock() {
+        return $this->stock;
+    }
+
+    public function getVat() {
+        return $this->vat;
+    }
+
+    public function getCatid() {
+        return $this->catid;
+    }
+
+    function setName($name) {
+        $this->name = $name;
+        return $this;
+    }
+
+    function setDescription($description) {
+        $this->description = $description;
+        return $this;
+    }
+
+    function setPrice($price) {
+        $this->price = $price;
+        return $this;
+    }
+
+    function setStock($stock) {
+        $this->stock = $stock;
+        return $this;
+    }
+
+    function setVat($vat) {
+        $this->vat = $vat;
+        return $this;
+    }
+
+    function setCatid($catid) {
+        $this->catid = $catid;
+        return $this;
+    }
+
+
+}
