14dbfe97f9e95525ee0498e4f4c5aae788ef7e43
tam24 <tam.escalante@gmail.com>
2017-12-12 17:06:07 +0100

    Creation of UserController and Form\User.php

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -1,18 +1,14 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 namespace Controller;
 
-/**
- * Description of UserController
- *
- * @author Etudiant
- */
+use Symfony\Component\HttpFoundation\Request;
+use Silex\Application;
+use Models\User;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Form\UserForm;
+use Models\Role;
+
 class UserController {
     //put your code here
 }
