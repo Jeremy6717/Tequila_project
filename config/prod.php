0f26fbcc9c5c1bc24ca808051c30f0d650a9f7d5
matthieu88160 <matthieu.vallance@cscfa.fr>
2017-12-15 16:37:01 +0100

    Resolve Jérémy errors

--- config/prod.php
+++ config/prod.php
@@ -4,7 +4,10 @@
 use Dflydev\Provider\DoctrineOrm\DoctrineOrmServiceProvider;
 use Silex\Provider\SecurityServiceProvider;
 use Symfony\Component\Security\Core\Encoder\PlaintextPasswordEncoder;
+use Silex\Provider\FormServiceProvider;
+use Silex\Provider\CsrfServiceProvider;
 
+$app['locale'] = 'en_en';
 $app['twig.path'] = array(__DIR__.'/../templates');
 $app['twig.options'] = array('cache' => __DIR__.'/../var/cache/twig');
 
@@ -64,6 +67,14 @@ $app->register(
     ]
 
 );
+
+$app->register(new Silex\Provider\ValidatorServiceProvider());
+$app->register(new FormServiceProvider());
+$app->register(new Silex\Provider\TranslationServiceProvider(), array(
+    'translator.domains' => array(),
+));
+$app->register(new CsrfServiceProvider());
+$app->register(new Silex\Provider\SessionServiceProvider());
 
 
 $app->register(new \Silex\Provider\SessionServiceProvider());
