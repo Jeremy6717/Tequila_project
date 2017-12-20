4a6f5f49f7795db4f0168a7286a132a5e8dc2faa
tam24 <tam.escalante@gmail.com>
2017-12-20 11:43:14 +0100

    Sign up working

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -18,57 +18,43 @@ use Silex\Provider\FormServiceProvider;
 class UserController {
 
     public function userSignupAction(Request $request, Application $app) {
-
-        $username = $request->request->get('usr_username');
-        $firstname = $request->request->get('usr_firstname');
-        $lastname = $request->request->get('usr_lastname');
-        $email = $request->request->get('usr_email');
-        $password = $request->request->get('usr_password');
-        $newsletter = $request->request->get('usr_newsletter');
-        $report = $request->request->get('usr_report');
-        $role = 'ROLE_ADMIN'; //$request->request->get('usr_role');   
-
-        /* $roleInstance = $app['orm.em']->getRepository(\Models\Role::class)->findOneByLabel($role);
-          if (!$role) {
-          throw new NotFoundHttpException('Role '.$role.' not found');
-          } */
-
-        $app->register(new FormServiceProvider());
+             
+        //If there are less than 5 then the user can be added
+        $user = new UserModel();
+       
         $formFactory = $app['form.factory'];
-        $userForm = $formFactory->create(UserForm::class); //, $user, ['standalone'=>true]);
+        $userForm = $formFactory->create(UserForm::class, $user, ['standalone'=>true]);
         $userForm->handleRequest($request);
-
+       
         if ($userForm->isSubmitted() && $userForm->isValid()) {
             $entityManager = $app['orm.em'];
+            
+            $role = $request->request->get('usr_role');
+            $roleRepository = $entityManager->getRepository(\Models\Role::class);
+            $userRole = $roleRepository->findOneByLabel('ROLE_ADMIN');
+            
+            if (!$role) { 
+                throw new NotFoundHttpException('Role '.$role.' not found');
+            }else{
+                $user->addRole($userRole);
+                
+                $entityManager = $app['orm.em'];
+                $entityManager->persist($user);
+                $entityManager->flush();
+                
+                return $app['twig']->render('signin.html.twig'); 
+            }    
 
-
-            $user = new UserModel();
-
-            $user->setFirstname($firstname)
-                    ->setLastname($lastname)
-                    ->setUsername($username)
-                    ->setPassword($password)
-                    ->setEmail($email)
-                    ->setNewsletter($newsletter)
-                    ->setReport($report)
-                    ->setRoles([$roleInstance]);
-
-            $entityManager = $app['orm.em'];
-            $entityManager->persist($user);
-            $entityManager->flush();
-
-            return $app->json($user->toArray());      
-
-
-            /*return $app['twig']->render(
+        }
+        
+        return $app['twig']->render(
                             'signUp.html.twig', [
                         'form' => $userForm->createView()
                             ]
-            );*/
-        }
-    }
+            );
+    } //end of function
 
-//end of function
-}
 
-//end of class UserController 
+} //end of class UserController 
+
+
